dfb41f64772ba586ac6d255f4c002b415ebfb43e
austin <aclyde11@gmail.com>
2019-04-26 10:19:50 -0500

    added tqdm

--- fast_jtnn/mol_tree.py
+++ fast_jtnn/mol_tree.py
@@ -2,6 +2,7 @@ import rdkit
 import rdkit.Chem as Chem
 from chemutils import get_clique_mol, tree_decomp, get_mol, get_smiles, set_atommap, enum_assemble, decode_stereo
 from vocab import *
+import pandas as pd
 from tqdm import tqdm
 class MolTreeNode(object):
 
@@ -115,12 +116,19 @@ if __name__ == "__main__":
     lg = rdkit.RDLogger.logger() 
     lg.setLevel(rdkit.RDLogger.CRITICAL)
 
+    df = pd.read_table(sys.argv[1], header=None)
+
     cset = set()
-    for line in tqdm(sys.stdin):
-        smiles = line.split()[0]
-        mol = MolTree(smiles)
-        for c in mol.nodes:
-            cset.add(c.smiles)
+    for i, row in tqdm(df.iterrows()):
+        smiles = row
+        print(row)
+        try:
+            mol = MolTree(smiles)
+            for c in mol.nodes:
+                cset.add(c.smiles)
+        except:
+            print("ERROR\t", smiles)
+
     for x in cset:
         print x
 
