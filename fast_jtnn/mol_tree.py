530f5fcd1c2746385916ef7dab089f3c0dbe7d1a
austin <aclyde11@gmail.com>
2019-04-26 14:47:39 -0500

    made parallel vocab

--- fast_jtnn/mol_tree.py
+++ fast_jtnn/mol_tree.py
@@ -141,7 +141,7 @@ if __name__ == "__main__":
         batch_size = int(sys.argv[4])
     print("Using ", jobs, " jobs. Specify job number after file name if this is wrong.")
     print("Loading file. File should have a single column with smiles. Errors will be printed.")
-    df = pd.read_csv(sys.argv[1], header=None, sep='\t')
+    df = pd.read_csv(sys.argv[1], header=None, sep='\t', names='SMILES')
     df = df.iloc[:10000, :]
     print("File loaded. Starting scan for bad smiles.")
 
