ea3e6be4c97714d8dbabe6f2048ed46bbe9ce5b9
austin <aclyde11@gmail.com>
2019-04-26 16:25:44 -0500

    made parallel vocab

--- fast_jtnn/mol_tree.py
+++ fast_jtnn/mol_tree.py
@@ -174,7 +174,7 @@ if __name__ == "__main__":
     print("scanning files")
     lock = Lock()
     p = Pool(processes=jobs, initializer=init, initargs=(lock,cset))
-    p.map(getVocab, tqdm(df.itertuples(index=False)))
+    p.map(getVocab, df.itertuples(index=False))
     p.close()
     p.join()
 
